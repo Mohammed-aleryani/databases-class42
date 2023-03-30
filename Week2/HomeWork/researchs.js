6cc2b11db2d4d2e8472ff43847899dfe8e3a73dd
Mohammed Aleryni <mym.aleryani@gmail.com>
2023-03-30 04:56:44 +0200

    Exercise 2 init version

--- Week2/HomeWork/researchs.js
+++ Week2/HomeWork/researchs.js
@@ -1,5 +1,5 @@
 import mysql from "mysql";
-import { authors,research_papers } from "./authersResearchSheets.js";
+import { authors, research_papers,authors_papers } from "./authersResearchSheets.js";
 const con = mysql.createConnection({
   host: "localhost",
   user: "hyfuser",
@@ -7,8 +7,8 @@ const con = mysql.createConnection({
   database: "userdb",
 });
 
-const executeSql = (sql,values) => {
-  con.query(sql,[values], (err, result, fields) => {
+const executeSql = (sql, values) => {
+  con.query(sql, [values], (err, result, fields) => {
     if (err) throw err;
     console.log(result);
   });
@@ -19,11 +19,25 @@ con.connect((err) => {
   console.log("Connected");
 });
 
+
+executeSql("DROP TABLE IF EXISTS authorS_papers");
+executeSql("DROP TABLE IF EXISTS research_papers ;");
+executeSql("SET FOREIGN_KEY_CHECKS=0");
 executeSql(
-  "CREATE TABLE IF NOT EXISTS research_papers ( paper_id INT AUTO_INCREMENT PRIMARY KEY,paper_title VARCHAR(255),conference VARCHAR(255),publish_date VARCHAR(255),author_id INT,FOREIGN KEY (author_id) REFERENCES authors(author_id));"
+  `INSERT INTO authors (author_name, university ,date_of_birth,h_index,gender,mentor) VALUES ?`,
+  authors
+);
+executeSql(
+  "CREATE TABLE IF NOT EXISTS research_papers ( paper_id INT AUTO_INCREMENT PRIMARY KEY,paper_title VARCHAR(255),conference VARCHAR(255),publish_date VARCHAR(255));"
+);
+executeSql("CREATE TABLE IF NOT EXISTS authors_papers (id INT AUTO_INCREMENT PRIMARY KEY,  author_id INT,paper_id INT ,FOREIGN KEY (author_id) REFERENCES authors(author_id)  ON DELETE CASCADE ,FOREIGN KEY (paper_id) REFERENCES research_papers(paper_id)  ON DELETE CASCADE)");
+executeSql(
+  `INSERT INTO research_papers (paper_title, conference ,publish_date) VALUES ?`,
+  research_papers
 );
 
-executeSql(`INSERT INTO authors (author_name, university ,date_of_birth,h_index,gender) VALUES ?`,authors)
-executeSql(`INSERT INTO research_papers (paper_title, conference ,publish_date,author_id) VALUES ?`,research_papers)
-
-
+executeSql(
+  `INSERT INTO authors_papers (author_id,paper_id) VALUES ?`,
+  authors_papers
+);
+executeSql("SET FOREIGN_KEY_CHECKS=1");
